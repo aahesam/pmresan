8c3f051fd9bb4924232e1d139a13a0e8cb4fa189
aahesam <45318539+aahesam@users.noreply.github.com>
2018-12-23 12:55:38 +0330

    Update set.php

--- set.php
+++ set.php
@@ -1,6 +1,6 @@
 <?php
-$API_KEY = '237966204:AAExLADwKL5KCi54dRF1nF7xLHH4dGCuewA';
-$hook_url = 'https://mrfenj.xyz/hoseinpor/bot/hoseinporbot.php';
+$API_KEY = '777113449:AAHrhhC6-NLYovqkPXWO72lCi0ZnhbKdxVo';
+$hook_url = 'https://aaahesam.000webhostapp.com/watermark/set.php';
 $hok="https://api.telegram.org/bot$API_KEY/setWebhook?url=$hook_url";
 echo file_get_contents($hok);
 
